39035df43f916d1c842a338482a768527adec869
zsl4753 <50951242+zsl4753@users.noreply.github.com>
2019-05-25 16:18:20 +0800

    第一次修改
    更新了新的算法，运算结果更快

--- 寻找“完美数”.py
+++ 寻找“完美数”.py
@@ -1,3 +1,6 @@
+'''
+初始代码，耗时较多
+'''
 def yueshu(x):
     ys = []
     sum = 0
@@ -12,3 +15,22 @@ def yueshu(x):
 for x in range(2,10000):
     if yueshu(x) == x:
 print(x)
+'''
+代码优化
+'''
+import time
+import math
+
+start = time.clock()
+
+for num in range(1,10000):
+    sum = 0
+    for factor in range(1,int(math.sqrt(num))+1):
+        if num%factor == 0:
+            sum =sum+factor
+            if factor >1 and num/factor !=factor:
+                sum = sum+ num/factor
+    if sum == num and num!= 1:
+        print(num)
+end = time.clock()
+print(end-start)
