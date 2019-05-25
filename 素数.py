a1e7fc54188d18ffeaba11c023a81c217077721c
zsl4753 <50951242+zsl4753@users.noreply.github.com>
2019-05-25 16:24:45 +0800

    第一次修改
    更新了算法

--- 素数.py
+++ 素数.py
@@ -1,3 +1,7 @@
+'''
+判断一个数是否为素数
+'''
+
 from math import sqrt
 num = int(input('请输入一个正整数:'))
 end = int(sqrt(num))
@@ -10,3 +14,16 @@ if is_prime and num!= 1:
     print("%d是素数"%num)
 else:
     print("%d不是素数"%num)
+
+'''
+找出从1-100间的素数
+'''
+import math
+for num in range(2,100):
+    is_prime = True
+    for factor in range(2,int(math.sqrt(num))+1):
+        if num%factor == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num)
