9aebb0d4bb82652e762018e455bd48388653a220
zsl4753 <50951242+zsl4753@users.noreply.github.com>
2019-05-25 13:01:25 +0800

    第一次更新
    简化了语言

--- 斐波那契.py
+++ 斐波那契.py
@@ -1,3 +1,17 @@
+'''
+输出斐波那契前20个数
+'''
+
+a = 0
+
+b = 1
+
+for _ in range(20):
+
+    (a, b) = (b, a + b)
+
+    print(a, end=' ')
+'''
 def fibo(x):
     if x == 1:
         return 1
@@ -10,3 +24,4 @@ def fibo(x):
 
 for i in range(10):
 print(fibo(i))
+'''
