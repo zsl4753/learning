dc03fdbf1a406af44ac5eafc01162339c1b033af
zsl4753 <50951242+zsl4753@users.noreply.github.com>
2019-05-25 12:59:25 +0800

    第一次更新
    增加了结束的条件

--- crap赌博游戏.py
+++ crap赌博游戏.py
@@ -4,6 +4,7 @@
 若和为其他点数，则记录第一次的点数和，玩家继续掷骰子，
 直至点数和等于第一次掷出的点数和则玩家胜；
 若掷出的点数和为7则庄家胜。
+开始有1000元，每次赌博200元
 '''
 
 import random
@@ -19,8 +20,10 @@ def crap():
     sum1 = x1 + x2
     if sum1 == 7 or sum1 == 11:
         print("玩家赢，掷出了%d和%d两个数，总和是%d"%(x1,x2,sum1))
+         return 1
     elif sum1 == 2 or sum1 == 3 or sum1 == 12:
         print("庄家赢，掷出了%d和%d两个数，总和是%d" % (x1, x2, sum1))
+        return 0
     else:
         print("第一次掷出了%d和%d两个数，总和是%d，不满足胜利要求，所以重新掷"%(x1,x2,sum1))
         sum2 = 0
@@ -32,8 +35,24 @@ def crap():
             n = n+1
         if sum2 == 7:
             print("庄家赢，在第%d次掷出了%d和%d两个数，总和是%d" % (n,x1, x2, sum2))
+            return 0
         else:
             print("玩家赢，在第%d次掷出了%d和%d两个数，总和是%d" % (n, x1, x2, sum2))
+             return 1
+money = 1000
+debt = 200
+while(money>0 and money <2000):
+    print("你的money是%d"%money)
+    num = crap()
+    if(num == 1):
+        money = money +debt
+    else:
+        money = money -debt
+
+if money <200:
+
+    print("你破产了,还剩%d"%(money))
+else:
+    print("你赚大钱了,现在有%d"%(money))
 
 
-crap()
